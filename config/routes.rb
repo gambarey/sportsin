936e2b89b50b75d829ee0a5065eefc5b9b83141b
Felipe Hernandez <felipe2eu@gmail.com>
2022-11-28 16:26:15 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users, only: %i[show] do
+    resources :offers, only: %i[new create show] do
+      resources :applications, only: %i[new index edit update]
+    end
+  end
+  resources :offers, only: %i[destroy index]
 end
