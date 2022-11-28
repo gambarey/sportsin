9ee2fe4414202467d5e028545607622428f110df
Felipe Hernandez <felipe2eu@gmail.com>
2022-11-28 17:44:05 +0100

    controllers

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -3,6 +3,16 @@ class OffersController < ApplicationController
   end
 
   def create
+    @user = User.find(params[:user_id])
+    @offer = offer.new(offer_params)
+    @offer.user = @user
+    @offer.user = current_user
+    @offer.status = false
+    if @offer.save
+      redirect_to offers_path, notice: 'offer was successfully created'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
