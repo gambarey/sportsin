2d91a8048b4bc4c842548859c4cbf7cc3ab1d581
Carlos Rodriguez <crribera@gmail.com>
2022-11-30 16:09:20 +0100

    applications index primitive

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,34 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_143304) do
     t.index ["user_id"], name: "index_achievements_on_user_id"
   end
 
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
   create_table "applications", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -93,12 +121,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_143304) do
   end
 
   add_foreign_key "achievements", "users"
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "applications", "offers"
   add_foreign_key "applications", "users"
   add_foreign_key "clubs", "sports"
-  add_foreign_key "offers", "clubs"
   add_foreign_key "messages", "clubs"
   add_foreign_key "messages", "users"
-  add_foreign_key "offers", "users"
+  add_foreign_key "offers", "clubs"
   add_foreign_key "users", "sports"
 end
