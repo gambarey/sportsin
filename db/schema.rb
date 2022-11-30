0ddf0f527c04e85064b76176219b30783182c0b1
ruben sanchez <sanchez.alvarez.ruben@gmail.com>
2022-11-30 15:30:30 +0100

    firstseeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_151835) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_29_134605) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,40 +24,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_151835) do
     t.index ["user_id"], name: "index_achievements_on_user_id"
   end
 
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum"
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
   create_table "applications", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
     t.bigint "offer_id"
-    t.integer "status"
+    t.bigint "user_id"
+    t.integer "status", default: 0
     t.index ["offer_id"], name: "index_applications_on_offer_id"
     t.index ["user_id"], name: "index_applications_on_user_id"
   end
@@ -82,8 +54,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_151835) do
     t.integer "fee"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_offers_on_user_id"
+    t.bigint "club_id"
+    t.index ["club_id"], name: "index_offers_on_club_id"
   end
 
   create_table "sports", force: :cascade do |t|
@@ -112,11 +84,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_151835) do
   end
 
   add_foreign_key "achievements", "users"
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "applications", "offers"
   add_foreign_key "applications", "users"
   add_foreign_key "clubs", "sports"
-  add_foreign_key "offers", "users"
+  add_foreign_key "offers", "clubs"
   add_foreign_key "users", "sports"
 end
