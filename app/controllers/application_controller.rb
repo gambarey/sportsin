2b6d22a3d18181b7646af98b983081593a37913b
Felipe Hernandez <felipe2eu@gmail.com>
2022-12-01 12:11:46 +0100

    somechanges

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :position, :role, :sport_id, :description])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :position, :role, :sport_id, :photo, :description])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :position, :role, :sport_id, :description])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :position, :role, :sport_id, :photo, :description])
   end
 end
