9c223835daa9b0503536b4449b4940b5fb76fafc
Carlos Rodriguez <crribera@gmail.com>
2022-12-01 16:20:44 +0100

    1st approach buttons

--- app/javascript/controllers/apply_controller.js
+++ app/javascript/controllers/apply_controller.js
@@ -1,9 +1,37 @@
 import { Controller } from "@hotwired/stimulus"
+static targets = ["more_info_btn", "side_pane"] // Add the new target
 
 // Connects to data-controller="apply"
 export default class extends Controller {
   connect() {
     console.log("hello from apply controller")
-  
+
   }
+displayInfo() {
+  @current_offer = this.
+}
+
+  update(event) {
+    // event.preventDefault()
+    const url = this.more_info_btnTarget.action
+    fetch(url, {
+      method: "GET",
+      headers: { "Accept": "text/plain" },
+      body: new FormData(this.more_info_btnTarget)
+    })
+      .then(response => response.text())
+      .then((data) => {
+        this.side_paneTarget.outerHTML = data
+      })
+  }
+
+  createApplication() {
+    const url = this.apply_btnTarget.action
+    fetch(url, {
+      method: "POST",
+      headers: { "Accept": "text/plain" },
+      body: new FormData(this.apply_btnTarget)
+    })
+  }
+  
 }
