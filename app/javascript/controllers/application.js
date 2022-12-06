423240eb471947b8bf915460f10ecfe869fc009a
Felipe Hernandez <felipe2eu@gmail.com>
2022-12-06 14:29:16 +0100

    js

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,5 +1,5 @@
 import { Application } from "@hotwired/stimulus"
-
+import ScrollProgress from 'stimulus-scroll-progress'
 const application = Application.start()
 
 // Configure Stimulus development experience
@@ -7,3 +7,22 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+const application = Application.start()
+application.register('scroll-progress', ScrollProgress)
+
+$(function(){
+  //WOW plugin init
+  new WOW().init();
+
+  //parallax effect for banner
+  (function() {
+      var posY;
+      var image = document.getElementById('parallax');;
+      function paralax() {
+          posY = window.pageYOffset;
+          image.style.top = posY * 0.9 + 'px';
+      }
+      window.addEventListener('scroll', paralax);
+  })();
+});
