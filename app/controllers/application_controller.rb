4b6ba3bd8f8d6a4c012bd196f9d2bfd457db941e
Carlos Rodriguez <crribera@gmail.com>
2022-12-07 11:11:04 +0100

    most demo ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :position, :role, :sport_id, :photo, :description])
   end
 
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
   protected
 
   def after_sign_out_path_for(resource_or_scope)
